a554b123765b74103989bfc433590349f14fbef0
air16gvu <Aidai.Rusalieva.gvu@fh-zwickau.de>
2025-03-11 17:17:56 +0100

    Budget fetching

--- src/main/java/com/example/ConfectionerWebsite/controllers/BudgetController.java
+++ src/main/java/com/example/ConfectionerWebsite/controllers/BudgetController.java
@@ -3,9 +3,13 @@ package com.example.ConfectionerWebsite.controllers;
 import com.example.ConfectionerWebsite.entities.Budget;
 import com.example.ConfectionerWebsite.services.BudgetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,9 +18,10 @@ public class BudgetController {
     private final BudgetService budgetService;
 
     @GetMapping("/budget")
-    public String getBudget(Model model) {
-        Budget budget = budgetService.getAllBudget().get(0);
-        model.addAttribute("budget", budget);
-        return "header"; // Имя шаблона FreeMarker
+    public ResponseEntity<Map<String, Double>> getTotalBudget() {
+        double totalBudget = budgetService.getAllBudget().get(0).getTotalAmount(); // Your logic to get budget sum
+        Map<String, Double> response = new HashMap<>();
+        response.put("budget", totalBudget);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/example/ConfectionerWebsite/services/BudgetService.java
+++ src/main/java/com/example/ConfectionerWebsite/services/BudgetService.java
@@ -2,14 +2,22 @@ package com.example.ConfectionerWebsite.services;
 
 import com.example.ConfectionerWebsite.entities.Budget;
 import com.example.ConfectionerWebsite.repositories.BudgetRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class BudgetService {
 
     private BudgetRepository budgetRepository;
 
     public Budget createBudget(Budget budget){
         return budgetRepository.save(budget);
+    }
+
+    public List<Budget> getAllBudget() {
+        return budgetRepository.findAll();
     }
 }
